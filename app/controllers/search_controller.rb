ff6054716ee41a1ab9d500869bb2b8809b365441
aMahdavian <amahdavian@live.com>
2015-04-13 17:40:24 +1000

    show entered search_term in search box on return Handle empty search term

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,15 +4,16 @@ class SearchController < ApplicationController
 
   def search
 
-    return if !params[:search_term].present?
-
-    photos = flickr.photos.search(
-        text:     params[:search_term],
+    if params[:search_term].present?
+      @search_term = params[:search_term]
+      photos = flickr.photos.search(
+        text:     @search_term,
         page:     params[:page] || 1,
         per_page: 30
       )
-    @search_results = WillPaginate::Collection.create(photos.page, photos.page, photos.pages) do |pager|
-       pager.replace(photos.to_a)
-    end
+      @search_results = WillPaginate::Collection.create(photos.page, photos.page, photos.pages) do |pager|
+        pager.replace(photos.to_a)
+      end
+    end    
   end
 end
