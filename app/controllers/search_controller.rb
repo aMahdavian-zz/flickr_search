2c880b309bd6b9a7431c6ce71b197779e04a518c
aMahdavian <amahdavian@live.com>
2015-04-13 12:59:24 +1000

    Add basic search functionality

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,5 +3,13 @@ class SearchController < ApplicationController
   end
 
   def search
+
+    return if !params[:search_term].present?
+
+    @search_results = flickr.photos.search(
+        text:     params[:search_term],
+        page:     1,
+        per_page: 30
+      )    
   end
 end
